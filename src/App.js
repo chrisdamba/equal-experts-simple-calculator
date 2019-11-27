e595921b477678d7728857811a85adefbd451ec2
Christopher Dambamuromo <chridam@gmail.com>
2019-11-27 13:07:47 +0000

    feat: add App file with assets and tests

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './assets/logo.png'
+
+import './assets/index.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <img alt="Equal Experts Logo" src={logo} className="logo" />
+      <h1>Equal Experts Calculator</h1>
+    </div>
+  )
+}
